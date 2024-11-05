f4eb8d177b0e599f18b235f4b0af0993acaa6dad
Salmaan Akhtar <akhtarsalmaan0@gmail.com>
2024-11-05 16:39:00 +0400

    added server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/qr_gen_att'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/qr_gen_att/dist/qr_gen_att/browser/index.html'));});
+app.listen(process.env.PORT || 8080);
